311fffdd87501ea97ea4883b2819d3f25f512592
Andrii Stohnii <stogniy.9797@gmail.com>
2025-09-07 18:29:11 +0300

    new: Complete homework 4.2

---
+++ HW_4.2/Program.cs
@@ -0,0 +1,47 @@
+﻿double firstItem = 0;
+double secondItem = 0;
+char mathOperation = '_';
+double result = 0;
+string errorText = "";
+
+Console.Write("Enter first number: ");
+double.TryParse(Console.ReadLine(), out firstItem);
+
+Console.Write("Enter second number: ");
+double.TryParse(Console.ReadLine(), out secondItem);
+
+Console.Write("Enter a mathematical operation: ");
+char.TryParse(Console.ReadLine(), out mathOperation);
+
+switch (mathOperation)
+{
+    case '+':
+        result = firstItem + secondItem;
+        break;
+    case '-':
+        result = firstItem - secondItem;
+        break;
+    case '*':
+        result = firstItem * secondItem;
+        break;
+    case '/':
+        if (secondItem == 0)
+        {
+            errorText = "ERROR: Division by zero is impossible";
+        }
+        else
+        {
+            result = firstItem / secondItem;
+        }
+        break;
+    default:
+        errorText = $"ERROR: Entered an undefined mathematical operation ({mathOperation}). Please try again.";
+        break;
+}
+
+if (errorText != "")
+{
+    Console.WriteLine(errorText);
+}
+else
+    Console.WriteLine($"Result:{result}");
