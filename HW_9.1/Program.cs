572e96ea0c0f3cc44c378412a14fcd929dcf74d2
Andrii Stohnii <stogniy.9797@gmail.com>
2025-09-25 23:24:35 +0300

    new: Complete homework 9.1

---
+++ HW_9.1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter Name and Surname: ");
+string input = Console.ReadLine();
+
+string[] words = input.Split(' ');
+
+string name = words[0];
+string surname = words[1];
+
+if (char.ToUpper(name[0]) == char.ToUpper(surname[0]))
+    Console.WriteLine("Last name start with the same letter as first name");
+
+else
+    Console.WriteLine("Last name does not start with the same letter as first name");
