eb3886047e949bd88240fa3ef961be6ede66e0e3
Andrii Stohnii <stogniy.9797@gmail.com>
2025-09-26 19:22:23 +0300

    new: Complete homework 9.3

---
+++ HW_9.3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter text with words separated by commas: ");
+string input = Console.ReadLine();
+
+string result = input.Replace(" ", "");
+
+Console.WriteLine("Result: " + result);
