e0aa939efb3c54ff76da60bd00f1f36a2a1717fc
Andrii Stohnii <stogniy.9797@gmail.com>
2025-09-26 19:17:25 +0300

    new: Complete homework 9.2

---
+++ HW_9.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+StringBuilder report = new StringBuilder();
+bool isLoopContinue = true;
+string input = System.String.Empty;
+int index = 1;
+
+report.AppendLine("============REPORT============");
+report.AppendLine("\nEvents:\n");
+
+
+
+do
+{
+    Console.Write("Enter event (or press \"Enter\" to finish): ");
+    input = Console.ReadLine();
+
+    if (input != "")
+    {
+        report.AppendLine($"{index,3}. {input}");
+        report.AppendLine($"     Date: {DateTime.Now.ToString("dd.mm.yyyy HH:mm:ss")}\n");
+        index++;
+    }
+    else
+        isLoopContinue = false;
+
+    Console.Clear();
+
+} while (isLoopContinue);
+
+Console.Write(report);
+
+
