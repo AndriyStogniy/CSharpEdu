9fff369b439266325b4d3c582389d7cfc7b3e915
Andrii Stohnii <stogniy.9797@gmail.com>
2025-10-05 16:39:31 +0300

    feat: Complete homework 11.1

---
+++ HW_11.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+int Fibonacci(int number)
+{
+    switch (number)
+    {
+        case 1:
+            return 0;
+        case 2:
+            return 1;
+        default:
+            return Fibonacci(number - 1) + Fibonacci(number - 2);
+
+    }  
+}
+
+int input = 0;
+
+Console.Write("Enter the Fibonacci sequence number: ");
+int.TryParse(Console.ReadLine(), out input);
+
+if (input > 0)
+    Console.WriteLine($"Fibonacci number is: {Fibonacci(input)}");
+else
+    Console.WriteLine("The number must be positive!");
