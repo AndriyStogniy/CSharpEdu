ff939e6aff8d9150dcfea6c3138161aa9c5067ef
Andrii Stohnii <stogniy.9797@gmail.com>
2025-10-15 20:12:08 +0300

    feat: Complete homework 13.1

---
+++ HW_13.1/Account.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_13._1
+{
+    class Account
+    {
+        private string accountName;
+        private double balance;
+
+        //get set
+        public string AccountName
+        {
+            get { return accountName; }
+        }
+        public double Balance
+        {
+            get { return balance; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("The initial balance cannot be less than 0!");
+                balance = value;
+            }
+        }
+        //Constructor
+        public Account(string accountName, double balance)
+        {
+            this.accountName = accountName;
+            if(balance < 0)
+                throw new ArgumentException("The initial balance cannot be less than 0!");
+
+            this.balance = balance;
+
+        }
+
+        //Class behavior
+        public void Withdrawal(double amount)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine("It isn't possible to deposit a negative amount!");
+                return;
+            }
+            if(balance - amount < 0)
+            {
+                Console.WriteLine("There are not enough funds in the account!");
+                return;
+            }
+            balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine("It isn't possible to deposit a negative amount!");
+                return;
+            }
+            balance += amount;
+        }
+
+    }
+}
---
+++ HW_13.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using HW_13._1;
+
+class Program
+{
+    static void Main()
+    {
+        Account PumbAccount = new Account("My Pumb account", 100.00);
+        Account MonoAccount = new Account("My Mono account", 1000000.00);
+
+        PumbAccount.Withdrawal(20);
+        Console.WriteLine($"The balance on {PumbAccount.AccountName} is now: {PumbAccount.Balance}");
+
+        MonoAccount.Deposit(200);
+        Console.WriteLine($"The balance on {MonoAccount.AccountName} is now: {MonoAccount.Balance}");
+    }
+}
