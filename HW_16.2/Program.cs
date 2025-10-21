de528fb7501877723280b94704e07f3d39f0398e
Andrii Stohnii <stogniy.9797@gmail.com>
2025-10-21 20:46:10 +0300

    feat: Complete homework 16.2

---
+++ HW_16.2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Enter number: ");
+string input = Console.ReadLine();
+
+try
+{
+    
+    decimal number = Convert.ToDecimal(input);
+    
+    Console.WriteLine($"You entered number: {number}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Error: The entered value is not a number!");
+}
+catch (OverflowException)
+{
+    Console.WriteLine("Error: The entered number is outside the allowable range!");
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+}
+
+
