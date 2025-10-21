53ac5405eb3b7fc2cfc79bc5f9f3999db0a862cb
Andrii Stohnii <stogniy.9797@gmail.com>
2025-10-21 20:29:53 +0300

    feat: Complete homework 16.1

---
+++ HW_16.1/Program.cs
@@ -0,0 +1,24 @@
+﻿int firstNumber = 0;
+int secondNumber = 0;
+int result = 0;
+
+try
+{
+    Console.Write("Enter first number: ");
+    firstNumber = int.Parse(Console.ReadLine());
+
+    Console.Write("Enter second number: ");
+    secondNumber = int.Parse(Console.ReadLine());
+
+    result = firstNumber / secondNumber;
+    Console.WriteLine("Result: ");
+}
+catch(DivideByZeroException ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+catch (FormatException)
+{
+    Console.WriteLine($"Number isn't correct");
+}
+
