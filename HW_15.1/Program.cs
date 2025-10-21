fc4b59ecffa17cd87070656403884d3b09a4b261
Andrii Stohnii <stogniy.9797@gmail.com>
2025-10-21 19:59:15 +0300

    feat: Complete homework 15.1

---
+++ HW_15.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the path to the source file: ");
+        string sourcePath = Console.ReadLine();
+
+        Console.Write("Enter the path to the file you want to copy the data to: ");
+        string destinationPath = Console.ReadLine();
+
+        try
+        {
+            if (!File.Exists(sourcePath))
+            {
+                Console.WriteLine("Error: Source file not found!");
+                return;
+            }
+
+            string content = File.ReadAllText(sourcePath);
+
+            File.WriteAllText(destinationPath, content);
+
+            Console.WriteLine("File copied successfully!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error copying file: {ex.Message}");
+        }
+    }
+}
